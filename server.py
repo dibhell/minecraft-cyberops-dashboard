83226ee5b399cccd7893d957e9ce544d4c8c947b
dibhell <ptr@o2.pl>
2026-09-11 12:34:08 +0200

    Disable stale dashboard asset caching

--- server.py
+++ server.py
@@ -682,6 +682,7 @@ class CyberHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-Type", content_type)
             self.send_header("Content-Length", str(len(data)))
+            self.send_header("Cache-Control", "no-store")
             self.end_headers()
             self.wfile.write(data)
         except Exception as e:
